24eb65c2c8aa413597cde5ce52c3a914623d89e8
Colin Schulte <pezhead53@gmail.com>
2018-02-01 12:28:34 -0600

    basic database implementation

--- LiftoffProject/LiftoffProject/Startup.cs
+++ LiftoffProject/LiftoffProject/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using LiftoffProject.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LiftoffProject
 {
@@ -22,8 +23,11 @@ namespace LiftoffProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GameDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddMvc();
-            services.AddTransient<GameDbContext>();
 
         }
 
